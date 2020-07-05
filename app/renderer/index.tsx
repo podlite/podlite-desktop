39babf5675e968af8bb9fcc0001c7d04c1405cf4
Aliaksandr Zahatski <zag@cpan.org>
2020-07-05 11:07:35 +0300

    clean

--- app/renderer/index.tsx
+++ app/renderer/index.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import Router from './router'
-import ExampleApp from './components/exampleapp'
-// import ExampleApp2 from './components/exampleapp2'
 import Editor from './components/editor'
-
-// const App = () => <div><span>{React.version} - </span><ExampleApp2 /></div>
 
 const render = (Component) => {
   ReactDOM.render(
@@ -15,6 +10,4 @@ const render = (Component) => {
   )
 }
 
-// render(Router)
-// render(ExampleApp)
 render(Editor)
