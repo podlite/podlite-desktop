4657bc407b9f68378462fd5aeab9ee822db013e2
Aliaksandr Zahatski <zag@cpan.org>
2021-11-14 21:08:16 +0300

    add cm plugin

--- app/renderer/components/App.tsx
+++ app/renderer/components/App.tsx
@@ -14,6 +14,7 @@ import { Rules } from '@podlite/schema';
 import {PODLITE_CSS} from '../utils/export-html'
 import './App.css';
 import '../../../node_modules/codemirror/lib/codemirror.css';
+import 'codemirror/addon/hint/show-hint';
 import '@podlite/editor-react/lib/index.css'
 
 import ReactDOM from 'react-dom';
