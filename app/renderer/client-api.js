07c742c1d58e4efc02e261c4628e493cd62f3af5
Alexander Zahatski <zag@cpan.org>
2025-10-12 10:06:02 +0200

    clean up unused

--- app/renderer/client-api.js
+++ app/renderer/client-api.js
@@ -1,6 +1,6 @@
 /* global vmd:true */
 
-const { ipcRenderer, remote } = require('electron')
+const { ipcRenderer } = require('electron')
 
 const electron = {
   ipc: ipcRenderer,
@@ -9,7 +9,6 @@ const electron = {
 // no var/let/const on purpose
 vmd = Object.assign(
   {
-    test: remote.app.getPath('userData'),
     windowid: require('url')
       .parse(global.location.href)
       .query.match(/id=(\d)+/)[1],
